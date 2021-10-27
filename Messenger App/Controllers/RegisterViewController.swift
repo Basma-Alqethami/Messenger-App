f69b9e5b85b7809f0a6d25955c935f6ba160dadd
Basma-Alqethami <91012377+Basma-Alqethami@users.noreply.github.com>
2021-10-28 00:12:01 +0300

    Create viewes

---
+++ Messenger App/Controllers/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  Messenger App
+//
+//  Created by Basma Alqethami on 21/03/1443 AH.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
